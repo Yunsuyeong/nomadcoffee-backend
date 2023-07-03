ed2986f02fa84204435536d4f37abb6d9e89b9be
윤수영 <yunsuyeong@yunsuyeong-ui-MacBookAir.local>
2023-07-03 15:41:17 +0900

    2023/07/03(Backend Settings)

---
+++ schema.js
@@ -0,0 +1,8 @@
+import { loadFilesSync, mergeTypeDefs, mergeResolvers } from "graphql-tools";
+
+const loadedTypes = loadFilesSync(`${__dirname}/**/*.typeDefs.js`);
+const loadedResolvers = loadFilesSync(
+  `${__dirname}/**/*.{queries,mutations}.js`
+);
+export const typeDefs = mergeTypeDefs(loadedTypes);
+export const resolvers = mergeResolvers(loadedResolvers);
